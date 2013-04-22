28f631434f1015e8c09432c3f3b6b65ffd8c8c1b
Pietro Gagliardi <pietro10@mac.com>
2013-04-21 20:29:06 -0400

    Derp I forgot to add print.go. :|

---
+++ print.go
@@ -0,0 +1,18 @@
+// 21 april 2013
+package main
+
+import (
+	"fmt"
+)
+
+func print() {
+	lbu32 := uint32(len(bytes))
+	for i := uint32(0); i < lbu32; i++ {
+		if label, ok := labels[i]; ok {
+			fmt.Printf("%s:\n", label)
+		}
+		if instruction, ok := instructions[i]; ok && instruction != operandString {
+			fmt.Printf("\t%s\n", instruction)
+		}
+	}
+}
