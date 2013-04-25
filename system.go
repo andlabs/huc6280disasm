bd1e95c5491e0d473c839861fa741773391dca2f
Pietro Gagliardi <pietro10@mac.com>
2013-04-25 18:08:23 -0400

    Changed system.go to use self-contained environments instead of a global one. The idea is to change branches and jumps to save and restore envirionments rather than press on with wrong page register values due to a jump that could not be followed or to close up some instructions after a branch to lots of code with the wrong page register values, but I will implement that later.

--- system.go
+++ system.go
@@ -12,70 +12,86 @@ type validbyte struct {
 	valid		bool
 }
 
-var a byte
-var a_valid bool
-
-var pages [8]struct {
-	which	byte
-	valid		bool
+type envt struct {
+	a		validbyte
+	pages	[8]validbyte
+	carryflag	validbyte
+	stack	[]validbyte
 }
 
-var carryflag int
-var carryflag_valid bool
+var env *envt
+
+func newenv() *evnt {
+	e := new(envt)
+	// TODO verify all this
+	e.a.valid = false
+	for i := 0; i < 7; i++ {
+		e.pages[i].valid = false
+	}
+	e.pages[7].value = 0x00		// we need the vectors at startup
+	e.pages[7].valid = true
+	e.carryflag.value = 0
+	e.carryflag.valid = false
+	return e
+}
 
 func init() {
-	// TODO verify all this
-	a_valid = false
-	for i := 0; i < 7; i++ {
-		pages[i].valid = false
-	}
-	pages[7].which = 0x00		// we need the vectors at startup
-	pages[7].valid = true
-	carryflag = 0
-	carryflag_valid = false
+	env = newenv()
 }
 
 func physical(logical uint16) (uint32, error) {
 	page := (logical & 0xE000) >> 13
-	if !pages[page].valid {
+	if !env.pages[page].valid {
 		return 0, fmt.Errorf("attempt to get physical address of logical $%X, but the page has not yet been initialized", logical)
 	}
 	physical := uint32(logical) &^ 0xE000
-	physical |= 0x2000 * uint32(pages[page].which)
+	physical |= 0x2000 * uint32(env.pages[page].value)
 	return physical, nil
 }
 
 func invalidate() {
-	a_valid = false
-	carryflag_valid = false
+	env.a.valid = false
+	env.carryflag.valid = false
 }
 
-var stack []validbyte
-
 func push(value byte, valid bool) {
-	stack = append(stack, validbyte{
+	env.stack = append(env.stack, validbyte{
 		value:	value,
 		valid:	valid,
 	})
 }
 
 func pop() (value byte, valid bool) {
-	if len(stack) == 0 {
+	if len(env.stack) == 0 {
 		return 0, false	// TODO correct?
 	}
-	t := stack[len(stack) - 1]
-	stack = stack[:len(stack) - 1]
+	t := env.stack[len(env.stack) - 1]
+	stack = env.stack[:len(env.stack) - 1]
 	return t.value, t.valid
 }
 
 func pusha() {
-	push(a, a_valid)
+	push(env.a, env.a.valid)
 }
 
 func pushinvalid() {
-	push(a, false)		// value of a irrelevant
+	push(env.a, false)		// value of a irrelevant
 }
 
 func popa() {
-	a, a_valid = pop()
+	env.a, env.a.valid = pop()
+}
+
+func saveenv() *envt {
+	e := new(envt)
+	e.a = env.a
+	e.pages = env.pages
+	e.carryflag = env.carryflag
+	e.stack = make([]validbyte, len(env.stack))
+	copy(e.stack, env.stack)
+	return e
+}
+
+func restoreenv(e *envt) {
+	env = e
 }
